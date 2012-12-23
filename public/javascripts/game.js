5c9343403bcfbe867d1b1e9643e0a6e6bf784987
mozair <macskeptic@gmail.com>
2012-12-23 13:17:03 -0200

    game loop up

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,3 +1,50 @@
 var SC = SC || {};
 
+SC.context = function () {
+  if (SC._context) {
+    return SC._canvas.getContext("2d");
+  }
+
+  SC._context =  $("foreground-canvas")[0].getContext("2d");
+
+  return SC._context;
+};
+
+SC.draw = function () {
+};
+
+SC.update = function (elapsed) {
+  console.log('update');
+  console.log(elapsed);
+};
+
+SC.handleInput = function () {
+};
+
+SC.requestAnimFrame = (function(callback) {
+  return window.requestAnimationFrame || 
+    window.webkitRequestAnimationFrame || 
+    window.mozRequestAnimationFrame || 
+    window.oRequestAnimationFrame || 
+    window.msRequestAnimationFrame ||
+    function(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
+SC.lastUpdate = 0;
+
+function animate() {
+  if(SC.lastUpdate == 0) {
+    SC.lastUpdate = new Date();
+  } else {
+    var currentUpdate = new Date();						
+    SC.update(currentUpdate - SC.lastUpdate);
+    SC.lastUpdate = currentUpdate;
+  }
+  SC.draw();
+  SC.requestAnimFrame.call(window, function() { animate(); });
+}
+
+$(animate);
 
