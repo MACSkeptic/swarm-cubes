09f8aa9da1629e6cc6d099fbb0a56fbb81df7a4a
mozair <macskeptic@gmail.com>
2012-12-23 19:59:05 -0200

    audio off

--- public/javascripts/audio.js
+++ public/javascripts/audio.js
@@ -1,10 +1,14 @@
 var SC = SC || {};
 SC.audio = {};
 
+SC.audio.off = true;
+
 SC.audio.spacegun = function () {
+  if (SC.audio.off) return;
   new Audio('/media/laser.wav').play();
 };
 
 SC.audio.background = function () {
+  if (SC.audio.off) return;
   new Audio('/media/digital-sonar.mp3').play();
 };
