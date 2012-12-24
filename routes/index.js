03526badb354db32417d6134f3b83f027dbb3d10
mozair <macskeptic@gmail.com>
2012-12-23 23:27:37 -0200

    added url from env var

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'swarm cubes', url: process.env.URL || 'http://localhost:3000' });
 };
